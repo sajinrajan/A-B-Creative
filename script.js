268c04728bfd2c5dc65ab2093db5cde73c0b1f56
sajinrajan <sajinrajan082@gmail.com>
2024-10-23 22:02:16 +0530

    SECONFD commit

--- script.js
+++ script.js
@@ -1,18 +1,64 @@
 
-// MOBIL RESPONSIVE
 const menuIcon = document.getElementById('menu-icon');
 const mobileDiv = document.getElementById('mobil-menu-bar');
 
-function openMenuBar(){
-mobileDiv.style.display = "flex"
-menuIcon.style.display = "none"   
+function openMenuBar() {
+    mobileDiv.classList.add('open');
 }
 
-function closeMenuBar(){   
-mobileDiv.style.display = "none"
-menuIcon.style.display = "flex"
+function closeMenuBar() {
+    mobileDiv.classList.remove('open');
 }
 
 
-// 
-/* <!-- SLIDER SHOW CONTACT START --> */
+
+/* First slider section start */
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+      loop: true,
+      margin: 20,
+      nav: false,
+      autoplay: false,
+      autoplayTimeout: 1000,
+      stagePadding: 50,
+      dots: true, 
+      responsive: {
+        0: {
+          items: 1 
+        },
+        600: {
+          items: 2 
+        },
+        1000: {
+          items: 5 
+        },
+        1400: {
+          items: 6 
+      }
+    }});
+  });
+/* First slider section end */
+  
+// <!-- OUR CLIENTS SECTION START -->
+
+$(".services").owlCarousel({
+  margin: 20,
+  loop: true,
+  autoplay: true,
+  autoplayTimeout: 2000,
+  autoplayHoverPause: true,
+  responsive: {
+      0: {
+          items: 1
+      },
+      600: {
+          items: 2
+      },
+      1000: {
+          items: 3
+      }
+  }
+});
+
+// <!-- OUR CLIENTS SECTION END -->
